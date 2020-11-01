83bb3889a8cf3130dee574eef2d8f37639074f29
Kawaguc <base1611ball@gmail.com>
2020-11-01 18:50:00 +0900

    疑似的なターミナルの処理 #4

---
+++ src/Terminal.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+  <h1>Terminal</h1>
+  <p>bossHP:{{num}}</p>
+  <p>positionIndex:{{positionIndex}}</p>
+  <p></p>
+  <input type="text"
+  v-model="textInput"
+  v-on:keydown.enter.exact.prevent
+  v-on:keyup.enter.exact="runCommand"
+  >
+  <button v-on:click="runCommand">runCommand</button>
+  <p>{{textInput}}</p>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "terminal",
+
+  data() {
+      return {
+          num: 10000,
+          textInput: '',
+          positionIndex: 1,
+      };
+  },
+  components: {
+    // HelloWorld
+  },
+  methods: {
+    runCommand() {
+      switch(this.textInput){
+        case 'attack':
+          this.attack();
+          break
+        default:
+          alert(`command ${this.textInput} not found`)
+          this.attack();
+      }
+    },
+    attack() {
+      this.num -= 200
+    }
+  }
+};
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Terminal from './Terminal.vue'
 
 createApp(App).mount('#app')
+createApp(Terminal).mount('#terminal')
