21ced1e941c294987e61f61987eb511e95cdb05c
Yasunari t <u7df25c588m737@gmail.com>
2020-11-04 22:03:08 +0900

    🎨style: Terminal とりあえず整形しました〜あとでもっとデザインします。

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,18 +1,25 @@
 <template>
-  <h1>Terminal</h1>
-  <p>parentbossHP:{{$parent.num}}</p>
-  <p>myHp: {{$parent.myHp}}</p>
-  <p>currentDir:{{$parent.currentPathForDisplay[$parent.currentDir]}}$</p>
-  <p>{{textInput}}</p>
-  <div v-for="(outputLine) in outputLines" v-bind:key="outputLine">{{outputLine}}</div>
-  <input type="text"
-  v-model="textInput"
-  v-bind:disabled="this.$parent.isEnemyTurn"
-  v-on:keydown.enter.exact.prevent
-  v-on:keyup.enter.exact="runCommand"
-  >
-  <button v-on:click="runCommand">runCommand</button>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="terminal">    
+    <p>parentbossHP:{{ $parent.num }}</p>
+    <p>myHp: {{ $parent.myHp }}</p>
+    <p>currentDir:{{ $parent.currentPathForDisplay[$parent.currentDir] }}$</p>
+    <p>{{ textInput }}</p>
+    <div v-for="outputLine in outputLines" v-bind:key="outputLine">
+      {{ outputLine }}
+    </div>
+    <div class="inputField">
+      <span>$</span>
+          <input
+        type="text"
+        v-model="textInput"
+        v-bind:disabled="this.$parent.isEnemyTurn"
+        v-on:keydown.enter.exact.prevent
+        v-on:keyup.enter.exact="runCommand"
+      />
+    </div>
+
+    <button v-on:click="runCommand">runCommand</button>
+  </div>
 </template>
 
 <script>
@@ -23,7 +30,12 @@ export default {
   data() {
     return {
       textInput: '',
-      outputLines: ['ここに', 'Terminalの画面に流す', 'Textを表示していく', 'あなたのターンです'],
+      outputLines: [
+        'ここに',
+        'Terminalの画面に流す',
+        'Textを表示していく',
+        'あなたのターンです',
+      ],
       outputLinesMaxLimit: 10,
     }
   },
@@ -49,18 +61,18 @@ export default {
       return parsedCommandsArray
     },
     runCommand() {
-      var parsedCommandsArray = this.parseCommand(this.textInput);
-      console.log(parsedCommandsArray);
+      var parsedCommandsArray = this.parseCommand(this.textInput)
+      console.log(parsedCommandsArray)
       this.updateLines(this.textInput)
-      var commandKind = parsedCommandsArray[0];
-      if (parsedCommandsArray.length >= 2){
-        var commandArg = parsedCommandsArray[1];
+      var commandKind = parsedCommandsArray[0]
+      if (parsedCommandsArray.length >= 2) {
+        var commandArg = parsedCommandsArray[1]
       }
-      console.log(commandKind);
-      switch(commandKind){
+      console.log(commandKind)
+      switch (commandKind) {
         case 'source':
-          this.source(commandArg);
-          this.checkIfContinue();
+          this.source(commandArg)
+          this.checkIfContinue()
           break
         case 'ls':
           this.ls()
@@ -83,32 +95,33 @@ export default {
           console.log(`command ${this.textInput} not found`)
       }
     },
-    source(commandArg){
-      if (this.$parent.isEnemyTurn === false){
+    source(commandArg) {
+      if (this.$parent.isEnemyTurn === false) {
         let sourceDir = this.$parent.currentDir
-        if (commandArg in this.$parent.armsPosition[sourceDir]){
+        if (commandArg in this.$parent.armsPosition[sourceDir]) {
           let armsDamage = this.$parent.armsPosition[sourceDir][commandArg]
-          this.$parent.num -= armsDamage;
+          this.$parent.num -= armsDamage
           this.updateLines(`${commandArg}を使った`)
-          this.updateLines(`bossに${armsDamage}のダメージ`);
+          this.updateLines(`bossに${armsDamage}のダメージ`)
           this.$parent.turnContinue = false
-        }
-        else{
-          this.updateLines('現在のファイルには指定した武器ファイルが存在しません')
+        } else {
+          this.updateLines(
+            '現在のファイルには指定した武器ファイルが存在しません'
+          )
           console.log('現在のファイルには指定した武器ファイルが存在しません')
         }
-      }
-      else if(this.$parent.isEnemyTurn === true){
+      } else if (this.$parent.isEnemyTurn === true) {
         console.log('your turn has not come yet')
       } else {
         console.log('isEnemyTurn typeerror')
         console.log(this.$parent.isEnemyTurn)
       }
-      this.$parent.turnContinue = false;
+      this.$parent.turnContinue = false
     },
     mkdir(dirName) {
       //FIX同名の場合はバグる
-      this.$parent.nextDirs[this.$parent.currentDir].push(dirName)    },
+      this.$parent.nextDirs[this.$parent.currentDir].push(dirName)
+    },
 
     cd(strPath) {
       if (strPath === '') {
@@ -149,73 +162,87 @@ export default {
       if (this.$parent.num <= 0) {
         this.$parent.num = 0
         this.updateLines('あなたの勝利です!')
-
-      }
-      else if(this.$parent.num > 0){
+      } else if (this.$parent.num > 0) {
         this.updateLines('Bossのターンです')
-        this.$parent.isEnemyTurn = true;
-        setTimeout(this.enemyTurn, 1000);
-      }
-      else {
+        this.$parent.isEnemyTurn = true
+        setTimeout(this.enemyTurn, 1000)
+      } else {
         console.log('changeTurnToEnemyがエラーを吐いています')
       }
     },
     //敵のターンにする処理はこの中に入れる
-    enemyTurn(){
+    enemyTurn() {
       if (this.$parent.isEnemyTurn == true) {
-      this.rm();
-      this.changeTurnToPlayer();
+        this.rm()
+        this.changeTurnToPlayer()
       }
     },
     rm() {
       //~が攻撃される確率をへらすためにrmPositionnにright とleftを増やしています
       let rmPosition = ['~', 'right', 'right', 'right', 'left', 'left', 'left']
-      let bossRmDamage = 200;
+      let bossRmDamage = 200
       if (this.$parent.isEnemyTurn === true) {
-        let whichToRm = rmPosition[Math.floor(Math.random() * rmPosition.length)]
+        let whichToRm =
+          rmPosition[Math.floor(Math.random() * rmPosition.length)]
         this.updateLines(`Bossは'${whichToRm}'以下のフォルダを攻撃した！`)
         if (whichToRm == '~') {
           this.updateLines(`playerに${bossRmDamage}のダメージ！`)
           console.log(`playerに${bossRmDamage}のダメージ！`)
-        }
-        else if (whichToRm ==  this.$parent.currentDir) {
-          this.$parent.myHp -= bossRmDamage;
+        } else if (whichToRm == this.$parent.currentDir) {
+          this.$parent.myHp -= bossRmDamage
           this.updateLines(`playerに${bossRmDamage}のダメージ！`)
           console.logs(`playerに${bossRmDamage}のダメージ！`)
-        }
-        else {
+        } else {
           this.updateLines('Bossの攻撃は外れた！')
         }
-
-      }
-      else if (this.$parent.isEnemyTurn === false) {
-        console.log('isEnemyTurn boolean error turn is not enemyTurn');
-      }
-      else{
-        console.log('isEnemyTurn type error');
+      } else if (this.$parent.isEnemyTurn === false) {
+        console.log('isEnemyTurn boolean error turn is not enemyTurn')
+      } else {
+        console.log('isEnemyTurn type error')
       }
     },
     changeTurnToPlayer() {
-      if(this.$parent.myHp <= 0) {
-        this.$parent.myHp = 0;
+      if (this.$parent.myHp <= 0) {
+        this.$parent.myHp = 0
         this.updateLines('あなたの敗北です')
-      }
-      else if(this.$parent.myHp > 0) {
+      } else if (this.$parent.myHp > 0) {
         this.updateLines('あなたのターンです')
-        this.$parent.isEnemyTurn = false;
+        this.$parent.isEnemyTurn = false
       }
     },
-    
+
     ls() {
       let childArray = this.$parent.nextDirs[this.$parent.currentDir]
-      for(let i = 0;i < childArray.length;i++){
+      for (let i = 0; i < childArray.length; i++) {
         this.updateLines(childArray[i])
       }
       this.updateLines(this.$parent.armsPosition[this.$parent.currentDir])
       console.log(this.$parent.nextDirs[this.$parent.currentDir])
-      this.$parent.turnContinue = true;
-    }
-
+      this.$parent.turnContinue = true
+    },
   },
 }
 </script>
+
+<style>
+.terminal {
+  max-width: 700px;
+  height: 250px;
+  margin: auto;
+  padding: 4px;
+  background: black;
+  border: 2px solid white;
+  border-radius: 4px;
+  text-align: left;
+  overflow: auto;
+}
+.terminal > * {
+  color: white;
+}
+.terminal > input {
+  background: none;
+}
+.inputField > span{
+  color: aqua;
+}
+</style>
--- src/views/Battle.vue
+++ src/views/Battle.vue
@@ -2,6 +2,7 @@
   <div class="battle">
     <!-- Note: GUI バトル画面 -->
     <TheBattleScreen />
+    <AppSpacer :height="36" />
     <!-- Note: バトルスクリーンの下にターミナルを表示する -->
     <Terminal />
   </div>
@@ -11,11 +12,13 @@
 // Components
 import Terminal from '../components/Terminal.vue'
 import TheBattleScreen from '../components/TheBattleScreen.vue'
+import AppSpacer from '../components/AppSpacer.vue'
 
 export default {
   components: {
     Terminal,
     TheBattleScreen,
+    AppSpacer,
   },
   data() {
     return {
@@ -31,17 +34,17 @@ export default {
         home: '/home',
       },
       arms: {
-        'sword':100,
-        'gun':1000,
-        'rock': 200,
-        'stick': 10,
-        'spear':250,
-        'hummer': 300
+        sword: 100,
+        gun: 1000,
+        rock: 200,
+        stick: 10,
+        spear: 250,
+        hummer: 300,
       },
       armsPosition: {
-        '~':{sword:100, gun:1000},
-        'left':{rock:200, stick:10},
-        'right':{spear:250, hummer:300}
+        '~': { sword: 100, gun: 1000 },
+        left: { rock: 200, stick: 10 },
+        right: { spear: 250, hummer: 300 },
       },
       parentDir: { '~': 'home', left: '~', right: '~' },
       linkedDirs: {
